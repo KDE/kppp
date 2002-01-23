a0a955231bbf39a11216aebaa7ce948ced946267
Per Winkvist <per.kde@gmail.com>
2002-01-23 20:15:59 +0000

    Add (c) notice
    svn path=/trunk/kdenetwork/kppp/; revision=133401

--- kpppwidget.h
+++ kpppwidget.h
@@ -1,3 +1,29 @@
+/*
+ *
+ *            kPPP: A pppd front end for the KDE project
+ *
+ * $Id$
+ *
+ *            Copyright (C) 1997 Bernd Johannes Wuebben
+ *                   wuebben@math.cornell.edu
+ *
+ *            Copyright (C) 1998-2002 Harri Porten <porten@kde.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #ifndef __KPPPWIDGET_H__
 #define __KPPPWIDGET_H__
 
