74489dd873d3828780fef19cbe1f404125a98897
Malte Starostik <malte@kde.org>
2002-03-12 18:08:00 +0000

    -  kapp->flushX(); +  KApplication::flushX();
    svn path=/trunk/kdenetwork/kppp/; revision=142620

--- miniterm.cpp
+++ miniterm.cpp
@@ -182,7 +182,7 @@ void MiniTerm::cancelbutton() {
 
   statusbar->setText(i18n("Hanging up ..."));
   kapp->processEvents();
-  kapp->flushX();
+  KApplication::flushX();
 
   Modem::modem->hangup();
 
@@ -197,7 +197,7 @@ void MiniTerm::resetModem() {
   statusbar->setText(i18n("Resetting Modem"));
   terminal->newLine();
   kapp->processEvents();
-  kapp->flushX();
+  KApplication::flushX();
 
   Modem::modem->hangup();
 
