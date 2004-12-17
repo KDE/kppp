b3d6481cdb878f2c792d6050313aea9db3de727b
Carsten Wolff <wolff@kde.org>
2004-12-17 15:30:32 +0000

    correctly hide/show the modem-combobox
    svn path=/branches/KDE_3_3_BRANCH/kdenetwork/kppp/; revision=371412

--- kpppwidget.cpp
+++ kpppwidget.cpp
@@ -112,6 +112,7 @@ KPPPWidget::KPPPWidget( QWidget *parent, const char *name )
   // l1->addWidget(label7, 1, 1); (done in resetmodems())
   modem_c = new QComboBox(false, this);
   label7->setBuddy(connectto_c);
+  m_bModemCShown = false;
 
   connect(modem_c, SIGNAL(activated(int)),
 	  SLOT(newdefaultmodem(int)));
@@ -487,13 +488,15 @@ void KPPPWidget::resetmodems() {
   }
 
   int newState = -1;
-  if (count > 1 && modem_c->isHidden()) {
+  if (count > 1 && !m_bModemCShown) {
       l1->addWidget(label7, 1, 1);
       l1->addWidget(modem_c, 1, 2);
+      m_bModemCShown = true;
       newState = 1;
-  } else if (modem_c->isShown()){
+  } else if (count <= 1 && m_bModemCShown){
       l1->remove(label7);
       l1->remove(modem_c);
+      m_bModemCShown = false;
       newState = 0;
   }
   if (newState >= 0) {
@@ -507,9 +510,8 @@ void KPPPWidget::resetmodems() {
   if(!gpppdata.defaultModem().isEmpty()) {
     for(int i=0; i < count; i++)
        if(gpppdata.defaultModem() == modem_c->text(i)) {
- 	modem_c->setCurrentItem(i);
-	gpppdata.setModemByIndex(i);
-
+          modem_c->setCurrentItem(i);
+          gpppdata.setModemByIndex(i);
     }
   }
   else
