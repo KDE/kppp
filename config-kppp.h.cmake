250a1042fe1679b98e70702b38621a1c5eeca7a7
Laurent Montel <montel@kde.org>
2006-05-14 11:51:35 +0000

    Move config
    svn path=/trunk/KDE/kdenetwork/kppp/; revision=540656

--- config-kppp.h.cmake
+++ config-kppp.h.cmake
@@ -2,3 +2,6 @@
 /* Define to 1 if you have the <linux/if_ppp.h> header file. */
 #cmakedefine HAVE_LINUX_IF_PPP_H 1
 
+/* Define to 1 if you have the <net/if_ppp.h> header file. */
+#cmakedefine HAVE_NET_IF_PPP_H 1
+
